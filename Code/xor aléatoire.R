da1ec56aa1de51a23bbc5bee2047aa37570d2a68
cprevosteau <clement.prevosteau@ensae.fr>
2014-05-30 08:39:51 +0200

    Changement David

--- Code/xor aléatoire.R
+++ Code/xor aléatoire.R
@@ -1,3 +1,5 @@
+# setwd("C:/Users/Clément/Documents/GitHub/GT/Code")
+#.libPaths("C:/Users/Clément/Documents/R-library")
 install.packages("kernlab")
 install.packages("neuralnet")
 
@@ -21,26 +23,31 @@ x=cbind(u,v)
 y <- 1*(U<0.5)
 x=cbind(x,y)
 colnames(x)=c("X","Y","Output")    	
-
+write.table(x,"XORSample.txt",row.names=F,col.names=F)
 
 
 couleur <- rep('red',n)
 couleur[y==1]<-'blue'
-plot(u,v, col=couleur)
+plot(u[1:100],v[1:100], col=couleur[1:100])
 title(main='Version aléatoire du Xor')
 
 #méthodes par noyaux
 library(kernlab)
 
 svp <- ksvm(x,y,type="C-svc",kernel="rbfdot")
+ypred=predict(svp,x[1:100,1:2])
+sum(ypred==y[1:100])
+plot(svp,data=x[1:100,1:2])
 plot(svp,data=x)
 title("SVM classification plot")
 
 svp <- ksvm(x,y,type="C-svc",kernel="vanilladot")
 plot(svp,data=x)
 
-svp <- ksvm(x,y,type="C-svc",kernel=polydot(degree=2))
-plot(svp,data=x)
+svp <- ksvm(x[1:100,1:2],y[1:100],type="C-svc",kernel=polydot(degree=2))
+ypred=predict(svp,x[1:100,1:2])
+sum(ypred==y[1:100])
+plot(svp,data=x[1:100,1:2])
 
 #RN
 #Régression logistique sur le xor aléatoire
@@ -62,12 +69,29 @@ print(net.results$net.result)
 
 
 
+####Résultats python
 
+nIter=5001
 
+errRiem=read.table("errRiem1.txt")
+errOrdR=rowMeans(errRiem)
 
+tRiem=as.matrix(read.table("tRiem1.txt"))
+tRiem=tRiem-rep(tRiem[1,],nIter)
+tAbsR=rowMeans(tRiem)
 
+errNat=read.table("errNat1.txt")
+errOrdN=rowMeans(errNat)
 
+tNat=as.matrix(read.table("tNat1.txt"))
+tNat=tNat-rep(tNat[1,],nIter)
+tAbsN=rowMeans(tNat)
 
+ymax=max(cbind(errOrdN,errOrdR))
+ymin=min(cbind(errOrdN,errOrdR))
+
+plot(tAbsR,errOrdR,type="l",col="red",ylim=c(ymin,ymax))
+lines(tAbsN,errOrdN,col="blue")
 
 
 
